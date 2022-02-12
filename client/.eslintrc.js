04a410afbb89a14d818fbf3b7413360dafc475af
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-02-12 03:14:02 +0100

    Added Lerna and Sanity Studio

---
+++ client/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:@next/next/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  },
+  globals: {
+    React: 'writable'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
